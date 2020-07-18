d044f65ead4071187b6e1138952f30e34d5f378a
Ivan Bazhov <ibazhov@gmail.com>
2020-07-18 14:47:25 +0100

    Insert Powerful Auto in main
    Add .clang-format file

--- PowerfulAuto.h
+++ PowerfulAuto.h
@@ -9,49 +9,49 @@
 #include <iostream>
 
 namespace cpp_tips_and_tricks {
-    namespace powerful_auto {
+namespace powerful_auto {
 
-        /// "Auto" can be used as the return type in function declaration.
-        /// More powerfully, function may return a struct which is declared
-        /// only inside the body of the function (no forward declaration needed).
-        /// We give two examples.
-        ///
-        /// Version: since C++17
-        ///
-        /// Sources:
-        /// https://en.cppreference.com/w/cpp/language/auto
-        ///
+/// "Auto" can be used as the return type in function declaration.
+/// More powerfully, function may return a struct which is declared
+/// only inside the body of the function (no forward declaration needed).
+/// We give two examples.
+///
+/// Version: since C++17
+///
+/// Sources:
+/// https://en.cppreference.com/w/cpp/language/auto
+/// https://youtu.be/iz5Qx18H6lg?t=1368
 
-        /// Example 1
-        auto doCalculation(double a, double b)
-        {
-            return a * b;
-        }
-
-        /// Example 2
-        auto makeComplexNumber(double real, double imaginary)
-        {
-            struct ComplexNumber {
-                double real_part;
-                double imaginary_part;
-            };
-
-            return ComplexNumber {real, imaginary};
-        }
-
-        /// Run examples
-        void doTrick()
-        {
-            auto product = doCalculation(2, 5);
-            assert (product == 10);
-
-            auto complex_number = makeComplexNumber(2, 5);
-            assert (complex_number.real_part == 2);
-            assert (complex_number.imaginary_part == 5);
-
-            std::cout << "Auto is powerful!" << std::endl;
-        }
-
-    }
+/// Example 1
+auto doCalculation(double a, double b)
+{
+    return a * b;
 }
 
+/// Example 2
+auto makeComplexNumber(double real, double imaginary)
+{
+    struct ComplexNumber
+    {
+        double real_part;
+        double imaginary_part;
+    };
+
+    return ComplexNumber{real, imaginary};
+}
+
+/// Run examples
+void doTrick()
+{
+    auto product = doCalculation(2, 5);
+    assert(product == 10);
+
+    auto complex_number = makeComplexNumber(2, 5);
+    assert(complex_number.real_part == 2);
+    assert(complex_number.imaginary_part == 5);
+
+    std::cout << "Auto is powerful!" << std::endl;
+}
+
+} // namespace powerful_auto
+} // namespace cpp_tips_and_tricks
--- main.cpp
+++ main.cpp
@@ -4,9 +4,12 @@
 // Created by Ivan Bazhov on 18/07/2020.
 //
 
+#include "PowerfulAuto.h"
+
 int main()
 {
-    // not tricks yet
+    
+    cpp_tips_and_tricks::powerful_auto::doTrick();
 
     return 0;
 }
